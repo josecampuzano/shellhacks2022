078eb2a82b29a67d73aecf626b1657ed62ca9ba1
Jose Campuzano <campuzanojoseluis14@gmail.com>
2022-09-09 22:46:54 -0700

    Added Components Pages and Layout folders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom"
+import Layout from "./Layout"
 
 function App() {
+  return (
+    <div>
+      <Switch>
+        <Route path="/">
+          <Layout />
+        </Route>
+      </Switch>
+    </div>
+  )
 
 }
 
